5597400093fac2de832c5364b08fe81feb13e9f0
Willian Reis <dev.wreis@gmail.com>
2021-10-11 22:28:00 -0300

    Configura Room Database

--- DioInterAndroidDeveloper/app/src/main/java/br/com/wmpr/diointerandroiddeveloper/App.kt
+++ DioInterAndroidDeveloper/app/src/main/java/br/com/wmpr/diointerandroiddeveloper/App.kt
@@ -1,6 +1,10 @@
 package br.com.wmpr.diointerandroiddeveloper
 
 import android.app.Application
+import br.com.wmpr.diointerandroiddeveloper.data.AppDatabase
+import br.com.wmpr.diointerandroiddeveloper.data.BusinessCardRepository
 
 class App : Application() {
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository (database.businessDao()) }
 }
--- DioInterAndroidDeveloper/app/src/main/java/br/com/wmpr/diointerandroiddeveloper/ui/AddBusinessCardActivity.kt
+++ DioInterAndroidDeveloper/app/src/main/java/br/com/wmpr/diointerandroiddeveloper/ui/AddBusinessCardActivity.kt
@@ -1,12 +1,21 @@
 package br.com.wmpr.diointerandroiddeveloper.ui
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import br.com.wmpr.diointerandroiddeveloper.App
+import br.com.wmpr.diointerandroiddeveloper.R
+import br.com.wmpr.diointerandroiddeveloper.data.BusinessCard
 import br.com.wmpr.diointerandroiddeveloper.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +28,18 @@ class AddBusinessCardActivity : AppCompatActivity() {
             finish()
         }
 
-        binding.btnConfirm.setOnClickListener {  }
+        binding.btnConfirm.setOnClickListener {
+            val businessCard = BusinessCard(
+                name = binding.tilName.editText?.text.toString(),
+                phone = binding.tilPhone.editText?.text.toString(),
+                company = binding.tilCompany.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                backgroundColor = binding.tilColor.editText?.text.toString().uppercase(),
+            )
+
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_sucess, Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
