c0d06f0601e42c9e24dde9499ea335a78b7429d5
Willian Reis <dev.wreis@gmail.com>
2021-10-16 15:15:30 -0300

    Cria o menu

--- DioInterAndroidDeveloper/app/src/main/java/br/com/wmpr/diointerandroiddeveloper/ui/MainActivity.kt
+++ DioInterAndroidDeveloper/app/src/main/java/br/com/wmpr/diointerandroiddeveloper/ui/MainActivity.kt
@@ -1,10 +1,14 @@
 package br.com.wmpr.diointerandroiddeveloper.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import br.com.wmpr.diointerandroiddeveloper.R
 import br.com.wmpr.diointerandroiddeveloper.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
@@ -15,4 +19,18 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return false
+    }
 }
